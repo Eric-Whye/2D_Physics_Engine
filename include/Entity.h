c04eeac34d2c00c87e4148d01667a35dbcf51123
Eric Whye (2K) <Eric.Whye@2k.com>
2024-01-26 17:30:46 +0000

    First Commit`

---
+++ include/Entity.h
@@ -0,0 +1,16 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+
+class Entity{
+    public:
+        Vector pos;
+        Entity(Vector position);
+        virtual ~Entity();
+
+    protected:
+
+    private:
+};
+
+#endif // ENTITY_H
